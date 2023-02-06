e96f63bbc974d793911bef8a1afd86ab29ae81cf
Ansar857 <64150961+Ansar857@users.noreply.github.com>
2023-02-06 15:55:13 +0500

    Add files via upload
    PIAIC assignment 02 guessing a number game

---
+++ index.ts
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+
+
+const response = await inquirer.prompt([
+    {
+        name: "number",
+        type: "number",
+        message: "Kindly enter any number"
+    }
+]);
+
+
+let a = response.number
+let b = Math.floor(Math.random()*10);
+//console.log(b)
+
+if ( a==b){
+    console.log("You Guess a Right Number "+ a)
+}
+else{
+    console.log("Your guess number is not right")
+}
